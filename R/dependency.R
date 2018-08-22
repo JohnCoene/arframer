5354bb20cb073a4495dd7e1c5724998f05c0fa88
JohnCoene <jcoenep@gmail.com>
2018-08-22 14:17:18 +0200

    improvements + new tag

--- R/dependency.R
+++ R/dependency.R
@@ -15,13 +15,13 @@
 #'
 #' a_scene(
 #'   a_dependency(), # aframe dependency
-#'   ar_dependency(), # arframer dependency
+#'   arframer_dependency(), # arframer dependency
 #'   arjs = NA
 #' )
 #'
 #' @rdname arframer
 #' @export
-ar_dependency <- function(cdn = FALSE){
+arframer_dependency <- function(cdn = FALSE){
   .get_dependency(
     script = "aframe-ar.js",
     cdn = cdn
@@ -33,7 +33,7 @@ ar_dependency <- function(cdn = FALSE){
 ar_scene <- function(..., cdn = FALSE){
   aframer::a_scene(
     aframer::a_dependency(cdn = cdn),
-    ar_dependency(cdn = cdn),
+    arframer_dependency(cdn = cdn),
     arjs = NA,
     ...
   )
@@ -45,3 +45,18 @@ embed_arframer <- function(ar){
   ar[[1]] <- htmltools::tagAppendAttributes(ar[[1]], embedded = NA)
   return(ar)
 }
+
+#' @rdname arframer
+#' @export
+ar_marker_camera <- function(...){
+  aframer::a_primitive(
+    "marker-camera",
+    list(
+      ...
+    )
+  )
+}
+
+#' @rdname arframer
+#' @export
+a_marker_camera <- ar_marker_camera
